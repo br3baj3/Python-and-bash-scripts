8a2e7b12493a56f35443da5cbe0ddd04367dc25e
br3baj3 <sinsentido@gmail.com>
2016-07-21 19:59:59 +0200

    Add files via upload
    This workaround is made up by 2 scripts: one in bash scripting for data retrieval and another one in python to compute de standard deviatino:
    
    - We have a list of routers being monitored by Nagios
    - First we obtain the list with the name of all the routers
    - Using this list and excluding the non desired ones, we retrieve the latency data monitored by Nagios from 8AM to 3PM. SSH is used for this purpose so the proper keys must be present in the machine running the script
    - For each router and its data, the welford.py script is invoked
    - Welford.py computes the standar deviation using an approach of the welford method, avoiding the "catastrophic cancellation" and making this computations more numerically stable.
    - Finally, if the standard deviation of the router is higher than 50ms, it is included in a list and an email with the results is sent to the network admin.
    
    The StandardDeviation.sh file could be included in a daily cron job.
    
    * Note: The welford.py script is basically

---
+++ StandardDeviation.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+VALUES="/var/tmp/routers_tmp.txt"
+VALUES_AUX="/var/tmp/VALUES_aux"
+WELFORD="/home/moebius/utils/welford.py"
+DEVIATION="/var/tmp/variance.txt"
+RECIPIENT="admin@domain.es"
+IP_NAGIOS=127.0.0.1
+
+# We calculate the start and end time in epoch format to make the request with rrdtool
+DATE=$(date +%Y%m%d)
+TIME_START=800
+TIME_END=1500
+TS_EPOCH=$(date -d "$DATE $TIME_START" +%s)
+TE_EPOCH=$(date -d "$DATE $TIME_END" +%s)
+
+# To obtain the list of routers first of all
+sudo ssh -c 3des $IP_NAGIOS "/usr/local/utilidades/inventario.pl -h --tipo router| cut -f 1 -d ';'| egrep "^r.*"">$VALUES
+
+# We obtain the necessary values to compute the standard deviation, removing the non desired columns and values to ease the conversion to float
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+        case $line in
+                routers|to|exclude)
+                ;;
+                *)
+            sudo ssh -n -c 3des $IP_NAGIOS "rrdtool fetch /usr/local/drraw/rrd/$line"_Latency.rrd" LAST -s $TS_EPOCH -e $TE_EPOCH | cut -f 2 -d ' '| awk 'NF > 0'| sed '/nan/d'|tr -s "," "."" >$VALUES_AUX 2>&1
+            /usr/bin/python $WELFORD "${line}">>$DEVIATION
+
+            rm $VALUES_AUX
+        esac
+
+done < "$VALUES"
+
+rm  `echo $VALUES`
+
+if [ -s $DEVIATION ]
+then
+        cat $DEVIATION | mail -s "Average and deviation in the latency of the routers (in ms)" $RECIPIENT
+fi
+
+rm $DEVIATION
